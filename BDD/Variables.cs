e487fbcfd80a861d56227ba1577b3bd1b0eb7bd9
olegy <olegy@DESKTOP-5D9SS61>
2023-08-12 23:08:22 +0300

    Refactored Wait methods, some small refactoring

--- BDD/Variables.cs
+++ BDD/Variables.cs
@@ -9,6 +9,4 @@ public class Variables
     public static string SubmitDataUrl = $"{Hooks.Hooks.Config.BaseUrl}/samplepagetest/";
     public static string ProgressBar = $"{Hooks.Hooks.Config.BaseUrl}/demo-site/progress-bar/";
     public static string DatePicker = $"{Hooks.Hooks.Config.BaseUrl}/demo-site/datepicker/";
-    public static string Username = "oleg.yablokov19";
-    public static string AccessKey = "onPaVaE6PJK4VEfBGTlhoovIAz9J3Jhp3kji4IEielQPbjb193";
 }
