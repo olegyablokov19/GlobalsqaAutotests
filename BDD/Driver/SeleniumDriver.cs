bd86f035ebc34b535870b05f12b3dfd5687ed8e9
olegyablokov19 <101104046+olegyablokov19@users.noreply.github.com>
2023-08-13 18:17:21 +0300

    Main branch local (#3)
    * Refactored Wait methods, some small refactoring
    
    * Added cross-browser tests
    
    * small refactor
    
    * small refactor
    
    ---------
    
    Co-authored-by: olegy <olegy@DESKTOP-5D9SS61>

--- BDD/Driver/SeleniumDriver.cs
+++ BDD/Driver/SeleniumDriver.cs
@@ -20,27 +20,6 @@ namespace BDD.Driver
 
         public IWebDriver Init(string browser)
         {
-            _driver = DriverFactory(browser);
-
-            //ChromeOptions capabilities = new ChromeOptions();
-            //capabilities.BrowserVersion = "114.0";
-            //Dictionary<string, object> ltOptions = new Dictionary<string, object>();
-            //ltOptions.Add("username", Variables.Username);
-            //ltOptions.Add("accessKey", Variables.AccessKey);
-            //ltOptions.Add("platformName", "MacOS Catalina");
-            //ltOptions.Add("resolution", "2048x1536");
-            //ltOptions.Add("project", "GlobalsQA");
-            //ltOptions.Add("build", "SomeBuild");
-            //ltOptions.Add("w3c", true);
-            //ltOptions.Add("plugin", "c#-nunit");
-            //capabilities.AddAdditionalOption("LT:Options", ltOptions);
-            //_driver = new RemoteWebDriver(new Uri("https://" + Variables.Username + ":" + Variables.AccessKey + "@hub.lambdatest.com/wd/hub"), capabilities.ToCapabilities());
-
-            return _driver;
-        }
-
-        private IWebDriver DriverFactory(string browser)
-        {
             switch (browser)
             {
                 case "Chrome":
@@ -56,6 +35,22 @@ namespace BDD.Driver
                     new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
                     return new ChromeDriver();
             }
+
+            //That could be used to run tests on Lambda Test or other cross-browser testing platform
+
+            //ChromeOptions capabilities = new ChromeOptions();
+            //capabilities.BrowserVersion = "114.0";
+            //Dictionary<string, object> ltOptions = new Dictionary<string, object>();
+            //ltOptions.Add("username", Variables.Username);
+            //ltOptions.Add("accessKey", Variables.AccessKey);
+            //ltOptions.Add("platformName", "MacOS Catalina");
+            //ltOptions.Add("resolution", "2048x1536");
+            //ltOptions.Add("project", "GlobalsQA");
+            //ltOptions.Add("build", "SomeBuild");
+            //ltOptions.Add("w3c", true);
+            //ltOptions.Add("plugin", "c#-nunit");
+            //capabilities.AddAdditionalOption("LT:Options", ltOptions);
+            //_driver = new RemoteWebDriver(new Uri("https://" + Variables.Username + ":" + Variables.AccessKey + "@hub.lambdatest.com/wd/hub"), capabilities.ToCapabilities());
         }
     }
 }
