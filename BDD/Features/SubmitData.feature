e70c050255ed760d00c3c02c75d76a09da282a33
olegyablokov19 <101104046+olegyablokov19@users.noreply.github.com>
2023-08-13 03:48:59 +0300

    Cross browser (#2)
    * Refactored Wait methods, some small refactoring
    
    * Added cross-browser tests
    
    ---------
    
    Co-authored-by: olegy <olegy@DESKTOP-5D9SS61>

--- BDD/Features/SubmitData.feature
+++ BDD/Features/SubmitData.feature
@@ -1,7 +1,13 @@
 ﻿Feature: SubmitData
 
-Scenario: Submitting the data
-	Given I've opened "Sample Page Test" page
+Scenario Outline: Submitting the data
+	Given I've opened "Sample Page Test" page in "<browser>" browser
 	When I fill all fields
 	And click Submit button
 	Then the correct information is displayed
+
+Examples: 
+| browser |
+| Chrome  |
+| Edge    |
+| Firefox |
--- BDD/Waiters.cs
+++ BDD/Waiters.cs
@@ -10,12 +10,19 @@ public class Waiters
     {
         var wait = new WebDriverWait(webdriver, TimeSpan.FromSeconds(3));
 
-        var adLocator = By.XPath("//ins[@data-anchor-status=\"displayed\"]");
-        IWebElement ad = wait.Until(x => x.FindElement(adLocator));
-        if (ad != null)
+        try
         {
-            executor.ExecuteScript("const a = document.getElementsByClassName(\"adsbygoogle adsbygoogle-noablate\");" +
-                                   "for (let i =0; i < a.length; i++) { a[i].style.height = \"0px\"; }");
+            var adLocator = By.XPath("//ins[@data-anchor-status=\"displayed\"]");
+            IWebElement ad = wait.Until(x => x.FindElement(adLocator));
+            if (ad != null)
+            {
+                executor.ExecuteScript("const a = document.getElementsByClassName(\"adsbygoogle adsbygoogle-noablate\");" +
+                                       "for (let i =0; i < a.length; i++) { a[i].style.height = \"0px\"; }");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("There was no ad");
         }
     }
 }
